1335cd17c1dfbc725cd84549e1fd86949b61c233
tuba balkan <tubabalkan165@gmail.com>
2024-03-05 21:44:17 +0300

    eklemeler yapıldı

--- TasteFoodit/Controllers/AdminLayoutController.cs
+++ TasteFoodit/Controllers/AdminLayoutController.cs
@@ -3,12 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using TasteFoodit.Context;
 
 namespace TasteFoodit.Controllers
 {
     public class AdminLayoutController : Controller
     {
-        // GET: AdminLayout
+        TasteContext context = new TasteContext();
         public ActionResult Index()
         {
             return View();
@@ -23,7 +24,9 @@ namespace TasteFoodit.Controllers
         }
         public PartialViewResult PartialNavbar()
         {
-            return PartialView();
+            ViewBag.notificationIsreadByFalseCount = context.Notifications.Where(x => x.IsRead == false).Count();
+            var values = context.Notifications.Where(x=>x.IsRead==false).ToList();
+            return PartialView(values);
         }
         public PartialViewResult PartialFooter()
         {
@@ -32,6 +35,14 @@ namespace TasteFoodit.Controllers
         public PartialViewResult PartialScript()
         {
             return PartialView();
+        }
+
+        public ActionResult NotificationStatusChangeToTrue(int id)
+        {
+            var values = context.Notifications.Find(id);
+            values.IsRead = true;
+            context.SaveChanges();
+            return RedirectToAction("CategoryList", "Category");
         }
     }
 }
---
+++ TasteFoodit/Controllers/HakkimizdaController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TasteFoodit.Controllers
+{
+    public class HakkimizdaController : Controller
+    {
+        // GET: Hakkimizda
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
