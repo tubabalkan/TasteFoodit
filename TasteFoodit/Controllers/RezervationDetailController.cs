724e122e1050fe30b689fb0fbb6de11ed3bd6ee5
tuba balkan <tubabalkan165@gmail.com>
2024-03-16 17:03:29 +0300

    ekleme yapıldı

--- TasteFoodit/Controllers/RezervationDetailController.cs
+++ TasteFoodit/Controllers/RezervationDetailController.cs
@@ -3,18 +3,31 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using TasteFoodit.Context;
+using TasteFoodit.Entities;
+
 
 namespace TasteFoodit.Controllers
 {
     public class RezervationDetailController : Controller
     {
-        // GET: RezervationDetail
+        TasteContext context = new TasteContext();
         public ActionResult Index()
         {
             return View();
         }
+        [HttpGet]
         public PartialViewResult PartialRezervasyon()
         {
+
+            return PartialView();
+        }
+      
+        [HttpPost]
+        public PartialViewResult PartialRezervasyon(Reservation r)
+        {
+            context.Reservations.Add(r);
+            context.SaveChanges();
             return PartialView();
         }
     }
